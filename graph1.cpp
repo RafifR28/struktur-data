31e9fcffbf2c567f5c57975ef746f0a1bdfd10fc
Rafif Ramadan <ramadanrafif30@gmail.com>
2022-12-07 07:57:41 +0700

    graph

---
+++ graph1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+struct peta
+{
+    int jarak;
+};
+
+struct peta sales[7][7] = {0, 7, 0, 5, 0, 0, 0,
+                           7, 0, 8, 9, 7, 0, 0,
+                           0, 8, 0, 0, 5, 0, 0,
+                           35, 34, 12, 0};
+
+int main()
+{
+    for (int x = 0; x < 4; x++)
+    {
+        for (int y = 0; y < 4; y++)
+        {
+            cout << "Inputkan Jarak dari " << x << " Ke " << y << " : ";
+            if (x == y)
+            {
+                sales[x][y].jarak = 0;
+                cout << sales[x][y].jarak << endl;
+            }
+            else if (x > y)
+            {
+                sales[x][y].jarak = sales[y][x].jarak;
+                cout << sales[x][y].jarak << endl;
+            }
+            else
+                cin >> sales[x][y].jarak;
+        }
+    }
+    // menampilkan data
+    for (int x = 0; x < 4; x++)
+    {
+        for (int y = 0; y < 4; y++)
+        {
+            cout << sales[x][y].jarak << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
